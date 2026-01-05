d09a9ce4ccd5fdee92b07aed5f84dcb1b4e24a54
Chris Marasco <cxm6467@gmail.com>
2026-01-05 03:28:18 -0500

    feat: start postgresql config

---
+++ config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Config(BaseSettings):
+    POSTGRES_PASSWORD: str
+    POSTGRES_USER: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore",
+        )
+
+    @property
+    def POSTGRES_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    
