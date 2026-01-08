50ebdedea72e139d8c00ca841a7096176cd3d45d
Chris Marasco <cxm6467@gmail.com>
2026-01-08 16:07:46 -0500

    feat: migration for tables

--- app/migrations/versions/502b598e8f50_add_items_table.py
+++ app/migrations/versions/502b598e8f50_add_items_table.py
@@ -20,9 +20,28 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    pass
+    op.create_table(
+        'store_managers',
+        sa.Column('id', sa.dialects.postgresql.UUID(as_uuid=True), primary_key=True, nullable=False),
+        sa.Column('name', sa.String(length=64), nullable=False),
+        sa.Column('email', sa.String(length=320), nullable=False),
+        sa.Column('password_hash', sa.String(), nullable=False)
+    )
+
+    op.create_table(
+        'items',
+        sa.Column('id', sa.dialects.postgresql.UUID(as_uuid=True), primary_key=True, nullable=False),
+        sa.Column('name', sa.String(length=64), nullable=False),
+        sa.Column('category', sa.String(length=32), nullable=False),
+        sa.Column('price_usd', sa.Float(), nullable=False),
+        sa.Column('in_stock', sa.Boolean(), nullable=False),
+        sa.Column('store_manager_id', sa.dialects.postgresql.UUID(as_uuid=True), sa.ForeignKey('store_managers.id'), nullable=False),
+    )
+
+
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    pass
+    op.drop_table('items')
+    op.drop_table('store_managers')
